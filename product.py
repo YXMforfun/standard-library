68a1829ffabfbc8637cf78e9c49f7353d377bb80
YXM <465184874@qq.com>
2016-07-08 23:47:16 +0800

    Update product.py

--- product.py
+++ product.py
@@ -19,6 +19,6 @@ def product(*args, repeat=1):
     pools = [tuple(pool) for pool in args] * repeat
     result = [[]]
     for pool in pools:
-        result = [x+[y]] for x in result for y in pool]
+        result = [x+[y] for x in result for y in pool]
     for prod in result:
         yield tuple(prod)
